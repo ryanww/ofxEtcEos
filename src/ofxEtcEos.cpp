c7af9b0523a8c06c8a7928fddfc205168ae20914
Ryan Wilkinson <ryanwilkinson@mac.com>
2017-08-29 13:02:35 -0400

    Added further setups

--- src/ofxEtcEos.cpp
+++ src/ofxEtcEos.cpp
@@ -1,15 +1,70 @@
 //
 //  ofxEtcEos.cpp
-//  wrvTitanicShowControl
 //
 //  Created by Ryan Wilkinson on 8/23/17.
 //
 
 #include "ofxEtcEos.h"
 
-void ofxEtcEos::setup(string _ip, int _port){
+//Init
+#pragma mark INIT
+ofxEtcEos::ofxEtcEos(){
+    ip = "";
+    port = 0;
+    twoWayMode = false;
+}
+ofxEtcEos::~ofxEtcEos(){
+    udpTx.Close();
+    udpRx.Close();
+    stopThread();
+}
+
+//Setup
+#pragma mark SETUP
+void ofxEtcEos::setup(string _ip, int _port, bool _twoWayMode){
+    ofLogVerbose("ofxEtcEos")<<"Initializing module with ip:"<<_ip<<":"<<_port<<" - two way mode:"<<_twoWayMode;
+    ip = _ip;
+    port = _port;
+    twoWayMode = _twoWayMode;
+    setupComs();
+    startThread();
+}
+void ofxEtcEos::setupComs(){
+    ofLogVerbose("ofxEtcEos")<<"Initializing coms";
+    //Setup UDP Transmitter
+    ofxUDPSettings settings;
+    settings.sendTo(ip, port);
+    settings.blocking = false;
+    udpTx.Setup(settings);
+    
+    if (twoWayMode){
+        //Setup UDP Receiver
+        udpRx.Create();
+        udpRx.Bind(51302);
+        udpRx.SetNonBlocking(true);
+    }
+    ofLogVerbose("ofxEtcEos")<<"Initializing coms complete";
+}
+
+//Running
+#pragma mark RUNNING
+void ofxEtcEos::threadedFunction(){
+    while (isThreadRunning()){
+        
+        
+        
+    }
+}
+
+
+//Sets
+#pragma mark SETS
+void ofxEtcEos::setEnabled(bool _enable){
     
 }
+
+
+
 
 void triggerCue(int _cuelist, int _cue){
     
--- src/ofxEtcEos.h
+++ src/ofxEtcEos.h
@@ -1,18 +1,29 @@
 //
-//  ofxEtcEos.hpp
-//  wrvTitanicShowControl
+//  ofxEtcEos.h
 //
 //  Created by Ryan Wilkinson on 8/23/17.
 //
 
 #include "ofMain.h"
+#include "ofxNetwork.h"
 
 #pragma once
 
 class ofxEtcEos : public ofThread {
   
 public:
-    void setup(string _ip, int _port);
+    //Init
+    ofxEtcEos();
+    ~ofxEtcEos();
+    
+    //Setup
+    void setup(string _ip, int _port, bool _twoWayMode = true);
+    
+    //Sets
+    void setEnabled(bool _enable);
+    void setUser(int _user);
+    
+    
     
     void triggerCue(int _cuelist, int _cue);
     
@@ -22,6 +33,17 @@ private:
     //Connection
     string ip;
     int port;
+    ofxUDPManager udpRx, udpTx;
+    bool twoWayMode;
+    void setupComs();
+    
+    //Running
+    void threadedFunction();
+    
+    //Console Specific Stuff
+    int user;
+    
+    
     
     
     
